995ff48f01ff232e4c0e259a82a911a4f6fbdd67
Cailum Leyshon <cailumleyshon@gmail.com>
2023-09-19 16:47:32 +0100

    Update .js .html .css

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-let userName = "";
+let userName = prompt("What is your username?");
 
-while (userName === "") {
-  userName = prompt("Enter a username please!");
-
-  if (userName === null) {
-    alert("You must enter a username to access this site.");
-  }
+if (userName.length <= 3) {
+  // console.log("Hello " + userName + "! That's a short user, but it'll do. Welcome to my site :)")
+  alert(
+    "Hello " +
+      userName +
+      "! That's a short user, but it'll do. Welcome to my site :)"
+  );
+} else if (userName.length <= 10) {
+  // console.log("Hello " + userName + "! Welcome to my site :)")
+  alert("Hello " + userName + "! Welcome to my site :)");
+} else {
+  // console.log("Hello " + userName + "! That's a long user, but it'll do. Welcome to my site :)")
+  alert(
+    "Hello " +
+      userName +
+      "! That's a long user, but it'll do. Welcome to my site :)"
+  );
 }
-
-console.log("Welcome " + userName + "! Nice to have you here.");
-alert("Welcome " + userName + "! Nice to have you here.");
