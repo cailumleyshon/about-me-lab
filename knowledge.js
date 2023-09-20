8c8e202addc5dc9bb5379f58b2c9fbe1bea88d3c
Cailum Leyshon <cailumleyshon@gmail.com>
2023-09-21 00:40:57 +0100

    Update knowledge.js refactoring code

--- knowledge.js
+++ knowledge.js
@@ -1,97 +1,86 @@
+let questions = [
+  {
+    question: "Is Cailum's favourite music Rock music?",
+    correctAnswer: "no",
+    correctResponse: "Correct answer! Well done champ.",
+    incorrectResponse:
+      "Incorrect answer. 90s hip-hop/pop was the right answer.",
+  },
+  {
+    question: "Does Cailum play the piano?",
+    correctAnswer: "yes",
+    correctResponse: "Correct answer! Well done champ.",
+    incorrectResponse: "Incorrect answer. I do play the piano.",
+  },
+  {
+    question: "Did Cailum go to University?",
+    correctAnswer: "yes",
+    correctResponse: "Correct answer! Well done champ.",
+    incorrectResponse: "Incorrect answer. I went to LJMU.",
+  },
+  {
+    question: "Is Cailum 22?",
+    correctAnswer: "no",
+    correctResponse: "Correct answer! Well done champ.",
+    incorrectResponse: "Incorrect answer. I'm 23.",
+  },
+  {
+    question: "Does Cailum live in Liverpool?",
+    correctAnswer: "yes",
+    correctResponse: "Correct answer! Well done champ.",
+    incorrectResponse: "Incorrect answer. I do live in Liverpool.",
+  },
+];
+
 let knowledgeScore = 0;
 
-let firstQuestion = prompt("Is Cailum's favourite music Rock music?");
-let firstQuestionLower = firstQuestion.toLowerCase();
+for (let i = 0; i < questions.length; i++) {
+  let userAnswer = prompt(questions[i].question).toLowerCase();
 
-if (firstQuestion === "no" || firstQuestion === "n") {
-  // console.log("Correct answer. Well done champ.");
-  alert("Correct answer! Well done champ.");
-  knowledgeScore++;
-} else if (firstQuestion === "yes" || "y") {
-  // console.log("Incorrect answer. 90s hip-hop/pop was the right answer.");
-  alert("Incorrect answer. 90s hip-hop/pop was the right answer.");
-} else {
-  console.log("Invalid input.");
-}
-
-let secondQuestion = prompt("Does Cailum play the piano?");
-let secondQuestionLower = secondQuestion.toLowerCase();
-
-if (secondQuestion === "no" || secondQuestion === "n") {
-  // console.log("Incorrect answer. I do play the piano.");
-  alert("Incorrect answer. I do play the piano.");
-} else if (secondQuestion === "yes" || "y") {
-  // console.log("Correct answer! Well done champ.");
-  alert("Correct answer! Well done champ.");
-  knowledgeScore++;
-} else {
-  // console.log("Invalid input.");
-}
-
-let thirdQuestion = prompt("Did Cailum go to University?");
-let thirdQuestionLower = thirdQuestion.toLowerCase();
-
-if (thirdQuestion === "no" || thirdQuestion === "n") {
-  // console.log("Incorrect answer. I went to LJMU.");
-  alert("Incorrect answer. I went to LJMU.");
-} else if (thirdQuestion === "yes" || "y") {
-  // console.log("Correct answer! Well done champ.");
-  alert("Correct answer! Well done champ.");
-  knowledgeScore++;
-} else {
-  // console.log("Invalid input.");
-}
-
-let fourthQuestion = prompt("Is Cailum 22?");
-let fourthQuestionLower = fourthQuestion.toLowerCase();
-
-if (fourthQuestion === "no" || fourthQuestion === "n") {
-  // console.log("Correct answer! Well done champ.");
-  alert("Correct answer! Well done champ.");
-  knowledgeScore++;
-} else if (fourthQuestion === "yes" || "y") {
-  // console.log("Incorrect answer. I'm 23.");
-  alert("Incorrect answer. I'm 23.");
-} else {
-  // console.log("Invalid input.");
-}
-
-let fifthQuestion = prompt("Does Cailum live in Liverpool?");
-let fifthQuestionLower = fifthQuestion.toLowerCase();
-
-if (fifthQuestion === "no" || fifthQuestion === "n") {
-  // console.log("Incorrect answer. I do live in Liverpool.");
-  alert("Incorrect answer. I do live in Liverpool.");
-} else if (fifthQuestion === "yes" || "y") {
-  // console.log("Correct answer! Well done champ.");
-  alert("Correct answer! Well done champ.");
-  knowledgeScore++;
-} else {
-  // console.log("Invalid input.");
+  if (userAnswer === questions[i].correctAnswer || userAnswer === "y") {
+    alert(questions[i].correctResponse);
+    knowledgeScore++;
+  } else if (userAnswer === questions[i].correctAnswer || userAnswer === "n") {
+    alert(questions[i].incorrectResponse);
+  } else {
+    alert("Invalid input.");
+  }
 }
 
 let maxGuesses = 4;
 let remainingGuesses = maxGuesses;
+const minNumber = 1;
+const maxNumber = 20;
+const correctNumber =
+  Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
 
-for (i = 0; i < maxGuesses; i++) {
-  let sixthQuestion = prompt(
-    "Random Trivia: How many countries are there in Europe? " +
+for (let i = 0; i < maxGuesses; i++) {
+  let userGuess = prompt(
+    "Guess the random number between " +
+      minNumber +
+      " and " +
+      maxNumber +
+      ". " +
       remainingGuesses +
-      " guesses left.)"
+      " guesses left."
   );
-  let sixthQuestionNumber = parseInt(sixthQuestion);
+  let userGuessNumber = parseInt(userGuess);
 
-  if (isNaN(sixthQuestionNumber)) {
-    alert("Invalid input. Please use numbers!");
+  if (
+    isNaN(userGuessNumber) ||
+    userGuessNumber < minNumber ||
+    userGuessNumber > maxNumber
+  ) {
+    alert("Invalid input. Please enter a number between 1 and 20.");
     remainingGuesses--;
-  } else if (sixthQuestionNumber < 44) {
+  } else if (userGuessNumber < correctNumber) {
     alert("Incorrect answer! Too low, go higher!");
     remainingGuesses--;
-  } else if (sixthQuestionNumber > 44) {
+  } else if (userGuessNumber > correctNumber) {
     alert("Incorrect answer! Too high, go lower!");
     remainingGuesses--;
   } else {
-    alert("Correct answer! How did you guess that?!");
+    alert("Correct answer! The random number was " + correctNumber + "!");
     knowledgeScore++;
     break;
   }
@@ -99,14 +88,16 @@ for (i = 0; i < maxGuesses; i++) {
 
 if (remainingGuesses === 0) {
   alert(
-    "You have run out of guesses. The answer was 44! Moving on to the next question."
+    "You have run out of guesses. The correct answer was " +
+      correctNumber +
+      ". Moving on to the next question."
   );
 }
 
 let maxGuesses2 = 6;
 let remainingGuesses2 = maxGuesses2;
 
-for (i = 0; i < maxGuesses2; i++) {
+for (let i = 0; i < maxGuesses2; i++) {
   let seventhQuestion = prompt(
     "What is one of the colours in a rainbow? " +
       remainingGuesses2 +
